b669f19a9af9ef324bd6312ba5ac1652c713ca6c
jbraekr <32002019+jbraekr@users.noreply.github.com>
2017-10-09 20:49:19 +0200

    uses .db/

--- db.js
+++ db.js
@@ -4,7 +4,11 @@ var PouchDB = require('pouchdb');
 var express = require('express');
 var app = express();
 
-app.use('/', require('express-pouchdb')(PouchDB,{
-}));
+app.use('/', require('express-pouchdb')(PouchDB.defaults({
+  prefix: '.db/',
+}), {
+    configPath: '.db/config.json',
+    logPath: '.db/log.txt',
+  }));
 
 app.listen(5984);

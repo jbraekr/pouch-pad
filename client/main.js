a8009c28327eb4a3d55d77a7efca7d5a77e79e68
jbraekr <32002019+jbraekr@users.noreply.github.com>
2017-10-10 14:54:45 +0200

    create/put/get works

--- client/main.js
+++ client/main.js
@@ -7,5 +7,22 @@ var db = new PouchDB('http://localhost:5984/kittens', {
 async function f() {
     var info = await db.info();
     console.log(info);
+    try {
+        var doc = await db.get("mittens");
+    } catch (e) {
+        console.log("catched", e);
+        var doc = {
+            "_id": "mittens",
+            "visited": new Date(),
+            "name": "Mittens",
+            "born": new Date(),
+        }
+    }
+    console.log(doc);
+    Object.assign(doc, {
+        "visited": new Date(),
+    });
+    await db.put(doc);
+    console.log(doc);
 }
 f();

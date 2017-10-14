d379362a987071be0a84fcf249f82f5b8b1cc2dd
jbraekr <32002019+jbraekr@users.noreply.github.com>
2017-10-14 13:44:22 +0200

    working url rewrite  with _utils

--- db.js
+++ db.js
@@ -15,6 +15,20 @@ var db = new TempPouchDB('kittens', { auto_compaction: true });
   Message: "MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 destroyed listeners added. Use emitter.setMaxListeners() to increase limit"
   */
 
+app.use(function (req, res, next) {
+  if (!/^\/db\//.test(req.url)) {
+    console.log(req.url);
+    if (/^\/_session$/.test(req.url)) {
+      console.log('session');
+      req.url = req.originalUrl = '/db/_session';
+    } else
+      if (/^\/$/.test(req.url)) req.url = req.originalUrl = '/db';
+      else req.url = req.originalUrl = '/db' + req.url;
+    console.log('->', req.url);
+  }
+  next();
+});
+
 app.use('/db',
   require('cors')({ origin: "*" }),
   require('express-pouchdb')(TempPouchDB, {
